c42b7ec2ebca0b3d745550188ec543f147c5ec21
Pedro <pedrokgsr@hotmail.com>
2025-05-17 12:16:04 -0300

    Alteração de conteúdos de mundo 1 para mundo 2

--- Mundo-01/CondicionaisAninhadas/condiçõesAninhadas3.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-
-if num1 > num2:
-    print("O primeiro número é maior")
-elif num2 > num1:
-    print("O segundo número é maior")
-elif num1 == num2:
-    print("Nenhum número é maior, ambos são iguais")
